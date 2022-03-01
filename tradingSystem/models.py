ab4ac10bb377a5606346ce7510003598f4458f61
wyl <961834635@qq.com>
2022-03-01 23:01:15 +0800

    完成前端基本框架

--- tradingSystem/models.py
+++ tradingSystem/models.py
@@ -5,26 +5,22 @@ from django.db import models
 
 class UserTable(models.Model):
     # 用户信息表
-    # 用户ID，
-    user_id = models.IntegerField(primary_key=True)
     # 身份证号码
-    id_no = models.CharField(max_length=18)
+    id_no = models.CharField(max_length=18,unique=True)
     # 用户名，用于显示和评论
     user_name = models.CharField(max_length=45)
     # 用户密码
     password = models.CharField(max_length=45)
-    # 用户性别
-    user_sex = models.CharField(max_length=5)
     # 用户电话号码，用于注册，开户，联系，PK
     phone_number = models.CharField(max_length=45,unique=True)
     # 用户邮箱
-    user_email = models.EmailField()
+    user_email = models.EmailField(unique=True)
     # 用户头像路径
     photo_url = models.CharField(max_length=45)
     # 银行卡号
-    account_num = models.CharField(max_length=45)
+    account_num = models.CharField(max_length=45,null=True)
     # 银行卡类型
-    account_type = models.CharField(max_length=45)
+    account_type = models.CharField(max_length=45,null=True)
     # 账户余额
     account_balance = models.FloatField(null=True)
     # 是否冻结
--- tradingSystem/views.py
+++ tradingSystem/views.py
@@ -12,7 +12,7 @@ def user_login(request):
         password = request.POST['password']
         user = UserTable.objects.get(phone_number=phone_number)
         if (user.password == password):
-            return render(request, 'index.html')
+            return render(request, 'base.html')
         else:
             return render(request, 'login.html')
 
@@ -27,12 +27,19 @@ def user_register(request):
         id_no = request.POST['id_no']
         user_email = request.POST['user_email']
         password = request.POST['password']
+        ensure_password = request.POST['ensure_password']
         account_type = request.POST['account_type']
         account_number = request.POST['account_number']
         message = ""
+        old_users = UserTable.objects.filter(phone_number=phone_number)
+        if password != ensure_password:
+            message = "输入的两次密码不一致！"
+            return render(request, 'register.html', locals())
+        elif old_users:
+            message = "该手机号已注册！"
+            return render(request, 'register.html', locals())
         try:
             user = UserTable.objects.create(
-                user_id=1,
                 user_name=user_name,
                 user_email=user_email,
                 user_sex=user_sex,
@@ -50,4 +57,10 @@ def user_register(request):
             print(Exception)
             message = "注册失败，请检查或稍后再试！"
             return render(request, 'register.html', locals())
-        return render(request,'register.html')
+        return render(request,'login.html')
+
+def index(request):
+    return render(request, 'base.html')
+
+def user_detail(request):
+    return render(request, 'user_detail.html')
